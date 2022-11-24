9d0dac7c010e68e3851603444f87400c08102f8d
blacktyger <blacktyg3r@gmail.com>
2022-11-24 15:35:02 +0000

    response function, cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -8,24 +8,31 @@ use epic_wallet_rust_lib::{
     EpicBoxConfig,
     open_wallet,
     tx_create,
+    tx_cancel,
     tx_post,
-    txs_get
+    txs_get,
 };
-
+use stack_test_epic_wallet_libwallet::{Error, ErrorKind};
 use pyo3::prelude::*;
 use serde_json;
+use serde_json::json;
+use uuid::Uuid;
+
+
+pub enum ResultData {
+    AsStringResult(Result<String, Error>),
+    AsVectorResult(Result<Vec<String>, Error>),
+    AsStringTuple((String, bool))
+}
 
 
 #[pyfunction]
-fn build_post_slate_request_py(
+fn post_request_py(
     config: String, receiver_address: String,
     slate: String, epicbox_config: String, password: String
-) -> PyResult<String> {
+) -> String {
 
-    //TODO: epic-box cfg changes, keep an eye
     let box_cfg = serde_json::from_str::<EpicBoxConfig>(&epicbox_config);
-    // let box_cfg = EpicBoxConfig::from_str(&epicbox_config).unwrap();
-
     let (wallet, keychain) =
         open_wallet(&config, &password.to_string()).unwrap();
     let key_pair =
@@ -37,84 +44,88 @@ fn build_post_slate_request_py(
         slate.to_string(),
         box_cfg.unwrap()
     );
-    Ok(slate_msg)
+    return result_response(ResultData::AsStringTuple((slate_msg, true)))
 }
 
-
 #[pyfunction]
-fn build_subscribe_request_py(
-    config: String, epicbox_config: String, password: String) -> PyResult<String> {
-
-    //TODO: cfg changes, keep an eye
+fn subscribe_request_py(
+    config: String, epicbox_config: String, password: String) -> String {
     let box_cfg = serde_json::from_str::<EpicBoxConfig>(&epicbox_config).unwrap();
-    // let box_cfg = EpicBoxConfig::from_str(&epicbox_config).unwrap();
-
     let (wallet, keychain) =
         open_wallet(&config, &password.to_string()).unwrap();
     let key_pair =
         get_wallet_secret_key_pair(&wallet, Some(keychain.unwrap()), 0);
     let subscribe_request =
         _build_subscribe_request(key_pair.unwrap(), box_cfg);
-    Ok(subscribe_request)
+
+    return result_response(ResultData::AsStringTuple((subscribe_request, true)))
 }
 
-
 #[pyfunction]
-fn decrypt_slates_py(config: String, password: String, encrypted_slates: String) -> PyResult<Vec<String>> {
+fn decrypt_slates_py(config: String, password: String, encrypted_slates: String) -> String {
     let (wallet, keychain) =
         open_wallet(&config, &password.to_string()).unwrap();
     let key_pair =
         get_wallet_secret_key_pair(&wallet, Some(keychain.unwrap()), 0);
     let decrypted_slates =
         decrypt_epicbox_slates(key_pair.unwrap(), &encrypted_slates);
-    Ok(decrypted_slates.unwrap())
+
+    return result_response(ResultData::AsVectorResult(decrypted_slates))
 }
 
-
 #[pyfunction]
-fn process_slate_py(config: String, password: String, slate: String) -> PyResult<String> {
+fn process_slate_py(config: String, password: String, slate: String) -> String {
     let (wallet, keychain) =
         open_wallet(&config, &password.to_string()).unwrap();
 
     let processed_slates = process_received_slates(
         &wallet, Some(keychain.unwrap()), &slate);
-    Ok(processed_slates.unwrap())
+
+    return result_response(ResultData::AsStringResult(processed_slates))
+
 }
 
-
 #[pyfunction]
-fn get_txs_py(config: String, password: String) -> PyResult<String> {
+fn create_tx_py(config: String, password: String, args: (u64, u64, bool)) -> String {
     let (wallet, keychain) =
         open_wallet(&config, &password.to_string()).unwrap();
-    let txs = txs_get(&wallet, keychain, true);
-    Ok(txs.unwrap())
+    let new_slate = tx_create(&wallet, keychain, args.0,
+              args.1, args.2);
+    return result_response(ResultData::AsStringResult(new_slate))
 }
 
-
 #[pyfunction]
-fn create_slate_py(config: String, password: String, args: (u64, u64, bool)) -> PyResult<String> {
-    let (wallet, keychain) =
-        open_wallet(&config, &password.to_string()).unwrap();
-    let create =
-        tx_create(&wallet, keychain, args.0,
-                  args.1, args.2).unwrap();
-
-    Ok(create)
-}
-
-
-#[pyfunction]
-fn tx_post_py(config: String, password: String, tx_slate_id: &str) -> PyResult<String> {
+fn post_tx_py(config: String, password: String, tx_slate_id: &str) -> String {
     let (wallet, keychain) =
         open_wallet(&config, &password.to_string()).unwrap();
     let post_tx = tx_post(&wallet, keychain, tx_slate_id);
 
-    Ok(post_tx.unwrap())
+    return result_response(ResultData::AsStringResult(post_tx))
 }
 
+#[pyfunction]
+fn get_txs_py(config: String, password: String) -> String {
+    let (wallet, keychain) =
+        open_wallet(&config, &password.to_string()).unwrap();
+    let txs = txs_get(&wallet, keychain, true);
+
+    return result_response(ResultData::AsStringResult(txs))
+}
 
 #[pyfunction]
-fn get_epicbox_address_py(config: String, password: String, domain: String, port: u16) -> PyResult<String> {
+fn cancel_tx_py(config: String, password: String, tx_slate_id: &str) -> String {
+    let (wallet, keychain) =
+        open_wallet(&config, &password.to_string()).unwrap();
+    let tx_slate_uuid = Uuid::parse_str(tx_slate_id).
+        map_err(|e| ErrorKind::GenericError(e.to_string())).unwrap();
+    let cancel =
+        tx_cancel(&wallet, keychain, tx_slate_uuid);
+
+    return result_response(ResultData::AsStringResult(cancel))
+}
+
+#[pyfunction]
+fn get_epicbox_address_py(config: String, password: String, domain: String, port: u16) -> String {
     let (wallet, keychain) =
         open_wallet(&config, &password.to_string()).unwrap();
     let key_pair =
@@ -122,19 +133,78 @@ fn get_epicbox_address_py(config: String, password: String, domain: String, port
     let address =
         get_epicbox_address(key_pair.unwrap().1, &domain, Some(port));
 
-    Ok(address.to_string())
+    return result_response(ResultData::AsStringTuple((address.to_string(), true)))
 }
 
 // wrap _py function and make them available as python package
 #[pymodule]
 fn epic_wallet_rust_python(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(build_post_slate_request_py, m)?)?;
-    m.add_function(wrap_pyfunction!(build_subscribe_request_py, m)?)?;
     m.add_function(wrap_pyfunction!(get_epicbox_address_py, m)?)?;
+    m.add_function(wrap_pyfunction!(subscribe_request_py, m)?)?;
     m.add_function(wrap_pyfunction!(decrypt_slates_py, m)?)?;
     m.add_function(wrap_pyfunction!(process_slate_py, m)?)?;
-    m.add_function(wrap_pyfunction!(create_slate_py, m)?)?;
-    m.add_function(wrap_pyfunction!(tx_post_py, m)?)?;
+    m.add_function(wrap_pyfunction!(post_request_py, m)?)?;
+    m.add_function(wrap_pyfunction!(create_tx_py, m)?)?;
+    m.add_function(wrap_pyfunction!(cancel_tx_py, m)?)?;
+    m.add_function(wrap_pyfunction!(post_tx_py, m)?)?;
     m.add_function(wrap_pyfunction!(get_txs_py, m)?)?;
     Ok(())
+}
+
+
+fn result_response(data: ResultData) -> String {
+    return match data {
+        ResultData::AsStringResult(result) => {
+            match result {
+                Ok(slate) => {
+                    serde_json::to_string(&json!({
+                    "error": 0,
+                    "message": "success",
+                    "result": slate
+                })).unwrap()
+                },
+
+                Err(err) => {
+                    serde_json::to_string(&json!({
+                    "error": 1,
+                    "message": err.to_string(),
+                    "result": 0
+                 })).unwrap()
+                }
+            }
+        },
+        ResultData::AsVectorResult(result) => {
+            match result {
+                Ok(slates) => {
+                    serde_json::to_string(&json!({
+                    "error": 0,
+                    "message": "success",
+                    "result": slates
+                })).unwrap()
+                },
+                Err(err) => {
+                    serde_json::to_string(&json!({
+                    "error": 1,
+                    "message": err.to_string(),
+                    "result": 0
+                 })).unwrap()
+                }
+            }
+        },
+        ResultData::AsStringTuple(result) => {
+            if result.1 {
+                serde_json::to_string(&json!({
+                    "error": 0,
+                    "message": "success",
+                    "result": result.0
+                })).unwrap()
+            } else {
+                serde_json::to_string(&json!({
+                    "error": 1,
+                    "message": result.0,
+                    "result": 0
+                })).unwrap()
+            }
+        }
+    }
 }
